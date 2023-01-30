eb05355d3f0d0196dc93f0c8ace15e65cca5e98a
zhivko.mihalev <zhivko.mihalev@pariplaydev.com>
2023-01-30 10:36:27 +0200

    Register car operators in ContainerConfig.

--- PariPlayCars.WebAPI/App_Start/ContainerConfig.cs
+++ PariPlayCars.WebAPI/App_Start/ContainerConfig.cs
@@ -11,6 +11,7 @@ using PariPlayCars.Data;
 using PariPlayCars.Services.DataServices;
 using PariPlayCars.Services.DataServices.Contracts;
 using PariPlayCars.Services.DataServices.Decorators;
+using PariPlayCars.Services.DataServices.Operators;
 using Repositories.GenericRepo;
 
 namespace PariPlayCars.WebAPI.App_Start
@@ -27,6 +28,11 @@ namespace PariPlayCars.WebAPI.App_Start
             builder.RegisterType<CarRepository>().As<ICarRepository>().InstancePerLifetimeScope();
             builder.RegisterType<CarService>().Named<ICarService>("original").InstancePerLifetimeScope();
             builder.RegisterType<CarServiceExceptionDecorator>().Named<ICarService>("decorator").InstancePerLifetimeScope();
+            builder.RegisterType<CarOperator>().As<ICarService>().InstancePerLifetimeScope();
+            builder.RegisterType<CarSearcher>().As<CarOperator>().InstancePerLifetimeScope();
+            builder.RegisterType<CarGetter>().As<CarOperator>().InstancePerLifetimeScope();
+            builder.RegisterType<CarRemover>().As<CarOperator>().InstancePerLifetimeScope();
+            builder.RegisterType<CarCreator>().As<CarOperator>().InstancePerLifetimeScope();
 
             builder.RegisterDecorator<ICarService>((c, inner) => c.ResolveNamed<ICarService>("decorator", TypedParameter.From(inner)), "original").InstancePerLifetimeScope();
 
